cca674cd6643c4c4caf4f94e7ba4b11dae1e1c1b
edo979 <prezentabilan@gmail.com>
2022-08-16 14:26:01 +0200

    basic fetching jobs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [jobs, setJobs] = useState([])
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const res = await fetch('data.json')
+      const data = await res.json()
+
+      setJobs(data)
+      console.log('fetched')
+    }
+
+    fetchJobs()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ul>
+      {jobs.map((job) => (
+        <li key={job.id}>{job.company}</li>
+      ))}
+    </ul>
+  )
 }
 
-export default App;
+export default App
