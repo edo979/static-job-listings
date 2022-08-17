84ebcbd0059404b28730d26963ee30c461944579
edo979 <prezentabilan@gmail.com>
2022-08-17 12:29:46 +0200

    basic job item layout

--- src/components/JobItem.jsx
+++ src/components/JobItem.jsx
@@ -1,5 +1,15 @@
-function JobItem({ data }) {
-  return <li key={data.id}>{data.company}</li>
+function JobItem({ data: { id, company, logo } }) {
+  return (
+    <li key={id}>
+      <div className="job | flex">
+        <div className="job_logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="job_info">{company}</div>
+        <div className="job_filter-list"></div>
+      </div>
+    </li>
+  )
 }
 
 export default JobItem
