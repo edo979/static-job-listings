64460b862158564c1b9e61827b4e42ab26594a69
edo979 <prezentabilan@gmail.com>
2022-08-18 22:11:04 +0200

    refactor reducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useEffect, useReducer, useState } from 'react'
 import JobCategories from './components/JobCategories'
 import JobList from './components/JobList'
+import JobReducer from './reducers/JobReducer'
 
 function App() {
   const initialState = { categories: [], filteredJobs: [], jobs: [] },
-    [state, dispatch] = useReducer(reducer, initialState)
+    [state, dispatch] = useReducer(JobReducer, initialState)
 
   useEffect(() => {
     const fetchJobs = async () => {
@@ -26,69 +27,6 @@ function App() {
 
     fetchJobs()
   }, [])
-
-  function reducer(state, action) {
-    switch (action.type) {
-      case 'INIT':
-        return {
-          ...state,
-          filteredJobs: [...action.payload],
-          jobs: [...action.payload],
-        }
-
-      case 'ADD-CATEGORY':
-        if (state.categories.includes(action.payload)) {
-          return state
-        } else {
-          const categories = [...state.categories, action.payload],
-            filteredJobs = filterJobs(categories, state.jobs)
-
-          return { ...state, categories, filteredJobs }
-        }
-
-      case 'REMOVE-CATEGORY':
-        const categories = state.categories.filter(
-            (category) => category !== action.payload
-          ),
-          filteredJobs = filterJobs(categories, state.jobs)
-        return { ...state, categories, filteredJobs }
-
-      case 'CLEAR-FILTER':
-        return {
-          categories: [],
-          filteredJobs: [...state.jobs],
-          jobs: state.jobs,
-        }
-
-      default:
-        break
-    }
-  }
-
-  function filterJobs(categories, jobs) {
-    const filteredjobs = jobs.filter((job) => {
-      const jobCategories = [
-        job.role,
-        job.level,
-        ...job.languages,
-        ...job.tools,
-      ]
-
-      let isIncludeCategory = true
-      for (let i = 0; i < categories.length; i++) {
-        if (jobCategories.includes(categories[i])) {
-          continue
-        } else {
-          isIncludeCategory = false
-          break
-        }
-      }
-
-      return isIncludeCategory
-    })
-
-    return filteredjobs
-  }
 
   return (
     <div>
---
+++ src/reducers/JobReducer.js
@@ -0,0 +1,57 @@
+export default (state, action) => {
+  switch (action.type) {
+    case 'INIT':
+      return {
+        ...state,
+        filteredJobs: [...action.payload],
+        jobs: [...action.payload],
+      }
+
+    case 'ADD-CATEGORY':
+      if (state.categories.includes(action.payload)) {
+        return state
+      } else {
+        const categories = [...state.categories, action.payload],
+          filteredJobs = filterJobs(categories, state.jobs)
+
+        return { ...state, categories, filteredJobs }
+      }
+
+    case 'REMOVE-CATEGORY':
+      const categories = state.categories.filter(
+          (category) => category !== action.payload
+        ),
+        filteredJobs = filterJobs(categories, state.jobs)
+      return { ...state, categories, filteredJobs }
+
+    case 'CLEAR-FILTER':
+      return {
+        categories: [],
+        filteredJobs: [...state.jobs],
+        jobs: state.jobs,
+      }
+
+    default:
+      break
+  }
+}
+
+function filterJobs(categories, jobs) {
+  const filteredjobs = jobs.filter((job) => {
+    const jobCategories = [job.role, job.level, ...job.languages, ...job.tools]
+
+    let isIncludeCategory = true
+    for (let i = 0; i < categories.length; i++) {
+      if (jobCategories.includes(categories[i])) {
+        continue
+      } else {
+        isIncludeCategory = false
+        break
+      }
+    }
+
+    return isIncludeCategory
+  })
+
+  return filteredjobs
+}
